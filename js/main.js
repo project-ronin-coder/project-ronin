771ea6443fb10c40e3a19e6d8e9d59a1c8031651
project-ronin-coder <ronvincent.sanjuan.rvsj@gmail.com>
2023-09-05 11:19:15 +0800

    -added animations on load and scroll
    -added animations to header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 const menu = document.querySelector("#mobile_menu");
 const menuLinks = document.querySelector(".navbar_list");
 const navLogo = document.querySelector("#navbar_logo");
+
+// For Scroll animation
+const observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else{
+      entry.target.classList.remove('show');
+    }
+  })
+})
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 // Display Mobile Menu
 const mobileMenu = () => {
   menu.classList.toggle("is-active");
